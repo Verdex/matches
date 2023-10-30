e6bbedc98691fd31d8efef5e3b723643069e4a01
David L. Inman <davidlevere@gmail.com>
2023-10-29 21:46:01 -0400

    handle patterns

--- src/main.rs
+++ src/main.rs
@@ -12,32 +12,39 @@ use structuralize::pattern::matcher::*;
 
 fn main() {
 
-    let mut args = std::env::args().collect::<Vec<_>>();
+    let args = std::env::args().collect::<Vec<_>>();
 
     if args.len() < 2 {
         eprintln!("usage: matches <pattern>+");
         return;
     }
 
-    args.reverse();
-    args.pop();
+    // TODO: add in index such that the error handling for pattern parser can indicate which 
+    // pattern had the problem.
+    let patterns : Result<Vec<_>, _> = args[1..].iter().map(|arg| to_pattern(arg)).collect();
 
-    let p = args.pop().unwrap();
-
-    let pattern : Pattern<SymStr> = p.parse().unwrap(); // TODO error handling?
-
-    let tcp = check_pattern(pattern).unwrap(); // TODO error handling?
+    if patterns.is_err() {
+        eprintln!("encountered problem parsing pattern: {}", patterns.unwrap_err());
+        return;
+    }
 
     let data = acquisition::get_data_from_dir().unwrap(); // TODO error handling?
 
-    for sub in data.to_lax() {
+    /*for sub in data.to_lax() {
         let results = pattern_match(&tcp, sub);
 
         for result in results {
             for (x, y) in result {
+                pattern_match(&tcp, y);
                 println!("{} {}", x, y);
             }
             println!("");
         }
-    }
+    }*/
+}
+
+fn to_pattern(arg : &str) -> Result<TypeChecked<SymStr>, Box<dyn std::error::Error>> {
+    let p : Pattern<SymStr> = arg.parse()?;
+    let tcp = check_pattern(p)?; 
+    Ok(tcp)
 }
